06539655ae119f3df7877d52ae89d4a7962ae9c7
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2024-11-13 11:54:23 +0530

    removing info
    Signed-off-by: Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -10,15 +10,10 @@ interface CustomResponse extends Omit<OpenAI.Chat.Completions.ChatCompletionMess
     prompt?: string;
 }
 
-// const client = new OpenAI({
-//     baseURL: process.env.OPENAI_BASE_URL,
-//     apiKey: process.env.OPENAI_API_KEY,
-// });
-
 const client = new OpenAI({
-    baseURL: "https://api.mistral.ai/v1",
-    apiKey: "DdcGRKnBET55js4ruWpCPoXDs0KJ30Ka",
-})
+    baseURL: process.env.OPENAI_BASE_URL,
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
 async function generateKeys() {
     console.log('Generating keys...');
