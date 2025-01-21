852f5d50d8fcdadd023ebb944a0c7cbe48fc3df4
Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>
2025-01-21 14:34:47 +0530

    [UI]: fixing deployment failure
    Signed-off-by: Ankita Sahu <71656941+SAHU-01@users.noreply.github.com>

--- src/component/ui/CoinCard.tsx
+++ src/component/ui/CoinCard.tsx
@@ -48,53 +48,52 @@ const Description: FC<{ text: string }> = ({ text }) => {
     );
 };
 
-export const CoinCard: FC<CoinCardProps> = React.memo(({ coin }) => {
+export const CoinCard: FC<CoinCardProps> = ({ coin }) => (
 
     // if (!coin.address) {
     //     return null;
     // }
-    return (
-        <Card
-            className={`text-left w-full cursor-pointer transition-colors relative overflow-hidden rounded-xl border-[#7E83A9] ${!coin.address ? 'cursor-not-allowed' : ''
-                }`}
-            onClick={() => {
-                if (coin.address) {
-                    // Redirect to the coin URL
-                    window.open(`https://pump.fun/coin/${coin.address}`, '_blank');
-                } else {
-                    // Show toast if the address is null
-                    toast.error('This coin has not been launched on pump.fun yet!', {
-                        duration: 3000,
-                    });
-                }
-            }}
-        >
-            <div className="absolute inset-0 bg-gradient-to-bl from-[#643ADE] via-[#070121] to-[#283081] hover:bg-[#3DF9FF]/20 opacity-95" />
-            <div className="absolute inset-0 backdrop-blur-sm" />
-            <CardContent className="relative z-10 p-4">
-                <div className="flex items-center gap-4">
-                    {/* Image */}
-                    <div className="relative w-16 h-16 flex-shrink-0">
-                        <Image
-                            src={coin.image || '/api/placeholder/40/40'}
-                            alt={coin.name}
-                            className="rounded-md object-cover"
-                            width={64}
-                            height={64}
-                        />
-                    </div>
 
-                    <div className="flex-1">
-                        <h3
-                            className="text-lg font-ttfirs bg-gradient-to-r from-[#A4C8FF] via-[#A992ED] to-[#643ADE] text-transparent bg-clip-text"
-                        >
-                            {coin.name}
-                        </h3>
-
-                        <Description text={coin.description} />
-                    </div>
+    <Card
+        className={`text-left w-full cursor-pointer transition-colors relative overflow-hidden rounded-xl border-[#7E83A9] ${!coin.address ? 'cursor-not-allowed' : ''
+            }`}
+        onClick={() => {
+            if (coin.address) {
+                // Redirect to the coin URL
+                window.open(`https://pump.fun/coin/${coin.address}`, '_blank');
+            } else {
+                // Show toast if the address is null
+                toast.error('This coin has not been launched on pump.fun yet!', {
+                    duration: 3000,
+                });
+            }
+        }}
+    >
+        <div className="absolute inset-0 bg-gradient-to-bl from-[#643ADE] via-[#070121] to-[#283081] hover:bg-[#3DF9FF]/20 opacity-95" />
+        <div className="absolute inset-0 backdrop-blur-sm" />
+        <CardContent className="relative z-10 p-4">
+            <div className="flex items-center gap-4">
+                {/* Image */}
+                <div className="relative w-16 h-16 flex-shrink-0">
+                    <Image
+                        src={coin.image || '/api/placeholder/40/40'}
+                        alt={coin.name}
+                        className="rounded-md object-cover"
+                        width={64}
+                        height={64}
+                    />
                 </div>
-            </CardContent>
-        </Card>
-    )
-});
+
+                <div className="flex-1">
+                    <h3
+                        className="text-lg font-ttfirs bg-gradient-to-r from-[#A4C8FF] via-[#A992ED] to-[#643ADE] text-transparent bg-clip-text"
+                    >
+                        {coin.name}
+                    </h3>
+
+                    <Description text={coin.description} />
+                </div>
+            </div>
+        </CardContent>
+    </Card>
+);
